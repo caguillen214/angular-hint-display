fe64c803b069022a45b6487089430c1e9e83bda2
Carlo s A. Guillen <caguillen@google.com>
2014-07-31 12:31:49 -0700

    setup(table-view): set up table view files

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
-angular.module('ngHintDisplays', []);
+angular.module('ngHintDisplay', ['ngRoute','ngTableView']).
+      config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+          controller: 'TableViewController',
+          controllerAs: 'tvCtrl',
+          templateUrl: 'components/table-view/table-view.html'
+        }).
+        otherwise({redirectTo: '/'});
+  }]);
---
+++ app/components/table-view/table-view-controller.js
@@ -0,0 +1,5 @@
+angular.module('ngTableView')
+  .controller('TableViewController', [
+    function(){
+
+  }]);
